093f4e41cf1fb655b0cd646921bb700e9ba491ba
Anjolaoluwa Olubusi <anjolaolubusi@gmail.com>
2022-01-01 18:08:38 -0500

    Added conversion from user data to model form

--- api_backend/src/events.rs
+++ api_backend/src/events.rs
@@ -1,9 +1,11 @@
+#![allow(non_snake_case)]
+
 use actix_web::{post, Result};
 use actix_web::web::{Json};
 use chrono::{DateTime, Utc};
 use serde::{Serialize, Deserialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct HardConstraint{
     pub class: String,
     pub end: DateTime<Utc>,
@@ -12,26 +14,25 @@ pub struct HardConstraint{
     pub title: String
 }
 
-impl HardConstraint {
-    pub fn new(class: String, end: DateTime<Utc>, source: String, start: DateTime<Utc>, title: String) -> Self{
-        Self{
-            class,
-            end,
-            source,
-            start,
-            title
-        }
-    }
-}
-
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct UserData{
     pub listOfEvents: Vec<HardConstraint>,
     pub monday: DateTime<Utc>,
     pub newEvent: RequestedEvent
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+impl UserData{
+    pub fn ConvertUserData(&self) -> Vec<ModelHardConstraint>{
+        let mut newVec = Vec::<ModelHardConstraint>::new();
+        for x in &self.listOfEvents{
+            newVec.push(ModelHardConstraint::new(x, self.monday));
+        }
+        return newVec;
+    }
+}
+
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct RequestedEvent{
     pub class: String,
     pub length: f32,
@@ -40,22 +41,25 @@ pub struct RequestedEvent{
     pub title: String    
 }
 
-impl RequestedEvent{
-    pub fn new(class: String, length: f32, selectedDate: DateTime<Utc>, source: String, title: String) -> Self{
+#[derive(Clone, Copy, Debug)]
+pub struct ModelHardConstraint{
+    pub end: f32,
+    pub start: f32,
+}
+
+impl ModelHardConstraint{
+    pub fn new(hc: &HardConstraint, monday: DateTime<Utc>) -> Self{
         Self{
-            class,
-            length,
-            selectedDate,
-            source,
-            title
+            end: (hc.end.timestamp() - monday.timestamp()) as f32/3600.0,
+            start: (hc.start.timestamp() - monday.timestamp()) as f32/3600.0,
         }
     }
 }
 
 #[post("/model")]
 async fn model(user_data: Json<UserData>) -> Result<String>{
-    println!("{:?}", user_data);
-    Ok(format!("{:?}", user_data))
+    let model_data:Vec<ModelHardConstraint> = user_data.ConvertUserData();
+    Ok(format!("{:?}", model_data))
     // Add Genetic Algorithm
     // Add ACO
 }
