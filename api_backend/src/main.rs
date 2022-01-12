ba8be076e53217b0ae4f921941a682e9649f598e
Anjolaoluwa Olubusi <anjolaolubusi@gmail.com>
2022-01-12 04:50:01 -0500

    API prints what url it is hosted at

--- api_backend/src/main.rs
+++ api_backend/src/main.rs
@@ -8,6 +8,11 @@ extern crate chrono;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()>{
+    let url = match cfg!(debug_assertions){
+        true => "127.0.0.1",
+        false => "backlogburner.com"
+    };
+    println!("Hosting URL: {}", url);
     HttpServer::new(|| {
         let cors = Cors::permissive();
         App::new()
@@ -15,7 +20,7 @@ async fn main() -> std::io::Result<()>{
             .service(endpoints::echo)
             .service(model::getNewSchedule)
     })
-    .bind(("127.0.0.1", 5000))?
+    .bind((url, 5000))?
     .run()
     .await
 }
